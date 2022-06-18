e53c0b8745d09e78929aaffc0c71071e48524e1d
Shakhzod Jumaev <shakhzod_jumaev@epam.com>
2022-06-18 13:40:29 +0500

    displaying images

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
+import { FaSearch } from "react-icons/fa";
+import Photo from "./Photo";
+
+// const clintID =
+//   "https://api.unsplash.com/photos/?client_id=9a_-yaBpIHAUdCmzGhJeV937rKzq3GsUTa2WqGfaKmM";
+// const myAccountApi = "9a_-yaBpIHAUdCmzGhJeV937rKzq3GsUTa2WqGfaKmM";
+
+const clientID = `?client_id=${process.env.REACT_APP_ACCESS_KEY}`;
+const mainUrl = "https://api.unsplash.com/photos";
+const searchUrl = "https://api.unsplash.com/search/photos";
 
 function App() {
+  const [loading, setLoading] = useState(false);
+  const [photos, setPhotos] = useState([]);
+  const fetchImages = async () => {
+    setLoading(true);
+    let url;
+    url = `${mainUrl}${clientID}`;
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      setPhotos(data);
+      setLoading(false);
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchImages();
+  }, []);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("hello");
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <section className="search">
+        <form className="search-form">
+          <input type="text" placeholder="search" className="form-input" />
+          <button type="submit" className="submit-btn" onClick={handleSubmit}>
+            <FaSearch />
+          </button>
+        </form>
+      </section>
+      <section className="photos">
+        <div className="photos-center">
+          {photos.map((photo, index) => {
+            console.log(photo);
+            return <Photo key={photo.id} {...photo} />;
+          })}
+        </div>
+        {loading && <h2 className="loading">Loading...</h2>}
+      </section>
+    </main>
   );
 }
 
