37517ee8b715d3a7217e26484df250ef3102b7ac
Shakhzod Jumaev <shakhzod_jumaev@epam.com>
2022-06-18 17:23:59 +0500

    search query is working well

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import { useEffect, useState } from "react";
 import { FaSearch } from "react-icons/fa";
 import Photo from "./Photo";
+import uuid from "react-uuid";
 
 // const clintID =
 //   "https://api.unsplash.com/photos/?client_id=9a_-yaBpIHAUdCmzGhJeV937rKzq3GsUTa2WqGfaKmM";
@@ -14,19 +15,35 @@ const searchUrl = "https://api.unsplash.com/search/photos";
 function App() {
   const [loading, setLoading] = useState(false);
   const [photos, setPhotos] = useState([]);
-  const [page, setPage] = useState(1);
+  const [page, setPage] = useState(0);
+  const [query, setQuery] = useState("");
 
   const fetchImages = async () => {
     setLoading(true);
     let url;
     const urlPage = `&page=${page}`;
-    url = `${mainUrl}${clientID}${urlPage}`;
+    const urlQuery = `&query=${query}`;
+
+    if (query) {
+      url = `${searchUrl}${clientID}${urlPage}${urlQuery}`;
+    } else {
+      url = `${mainUrl}${clientID}${urlPage}`;
+    }
+
     try {
       const response = await fetch(url);
       const data = await response.json();
+
       setPhotos((oldPhotos) => {
-        return [...oldPhotos, ...data];
+        if (query && page === 1) {
+          return data.results;
+        } else if (query) {
+          return [...oldPhotos, ...data.results];
+        } else {
+          return [...oldPhotos, ...data];
+        }
       });
+      console.log(data);
       setLoading(false);
     } catch (error) {
       setLoading(false);
@@ -37,11 +54,6 @@ function App() {
   useEffect(() => {
     fetchImages();
   }, [page]);
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log("hello");
-  };
 
   useEffect(() => {
     const event = window.addEventListener("scroll", () => {
@@ -57,11 +69,22 @@ function App() {
     return () => window.removeEventListener("scroll", event);
   }, []);
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setPage(1);
+  };
+
   return (
     <main>
       <section className="search">
         <form className="search-form">
-          <input type="text" placeholder="search" className="form-input" />
+          <input
+            type="text"
+            placeholder="search"
+            className="form-input"
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
+          />
           <button type="submit" className="submit-btn" onClick={handleSubmit}>
             <FaSearch />
           </button>
@@ -69,9 +92,9 @@ function App() {
       </section>
       <section className="photos">
         <div className="photos-center">
-          {photos.map((photo, index) => {
+          {photos.map((photo) => {
             console.log(photo);
-            return <Photo key={photo.id} {...photo} />;
+            return <Photo key={uuid()} {...photo} />;
           })}
         </div>
         {loading && <h2 className="loading">Loading...</h2>}
