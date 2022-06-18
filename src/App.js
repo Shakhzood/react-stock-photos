ddee2dda505f55c660535a5489cb4fe8c6fc609c
Shakhzod Jumaev <shakhzod_jumaev@epam.com>
2022-06-18 14:17:54 +0500

    useEffect clean up function

--- src/App.js
+++ src/App.js
@@ -36,6 +36,19 @@ function App() {
     e.preventDefault();
     console.log("hello");
   };
+
+  useEffect(() => {
+    const event = window.addEventListener("scroll", () => {
+      if (
+        window.innerHeight + window.scroll >=
+        document.body.scrollHeight - 1
+      ) {
+        console.log("it worked");
+      }
+    });
+    return () => window.removeEventListener("scroll", event);
+  }, []);
+
   return (
     <main>
       <section className="search">
